ee92c714462f83390ebb8927bd3ca47dc2f62438
ogrodek <70914232+KrzysztofOGit@users.noreply.github.com>
2022-01-31 14:44:25 +0000

    change polygon area to svg

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const imageMap = document.getElementById('imageMap');
+const imageMap = document.querySelector('svg');
 
 imageMap.addEventListener('click', materialChange);
 
 function materialChange(el) {
-	console.log(el.target)
+	console.log(el.target )
 	
 }
+
+/*
+function getNode(n, v) {
+  n = document.createElementNS("http://www.w3.org/2000/svg", n);
+  for (var p in v)
+    n.setAttributeNS(null, p.replace(/[A-Z]/g, function(m, p, o, s) { return "-" + m.toLowerCase(); }), v[p]);
+  return n
+}
+
+var svg = getNode("svg");
+document.body.appendChild(svg);
+
+var r = getNode('rect', { x: 131, y: 124, width: 461, height: 150, fill:'#ff00ff' });
+svg.appendChild(r);
+*/
 
