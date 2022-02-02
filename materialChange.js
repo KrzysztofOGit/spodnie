7c15859b501450ee560a21b3c8f15db8be3159ea
ogrodek <70914232+KrzysztofOGit@users.noreply.github.com>
2022-02-02 11:10:45 +0000

    type="module"

---
+++ materialChange.js
@@ -0,0 +1,6 @@
+//chose color for the part fo the trouser
+let message = function materialChange(el) {
+	return console.log(el.target.id )
+}
+
+export default message;
--- script.js
+++ script.js
@@ -1,35 +1,14 @@
+import message from './materialChange.js';
+import openModalWindow  from './modalWindowChoosematerial.js';
+
 const imageMap = document.querySelector('svg');
 
-imageMap.addEventListener('click', materialChange);
-
-//chose color for the part fo the trouser
-function materialChange(el) {
-	console.log(el.target.id )
-}
-
-// Get the modal
-var modal = document.getElementById("myModal");
+imageMap.addEventListener('click', message);
 
 // Get the button that opens the modal
 var btnSVG = document.querySelector('svg');
-//var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btnSVG.onclick = function() {
-  modal.style.display = "block";
-}
+btnSVG.addEventListener('click', openModalWindow )
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
